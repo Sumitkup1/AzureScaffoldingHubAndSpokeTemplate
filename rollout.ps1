ae8ef1b954226f055f67c8c0807dea5ee517aed9
sumitkup1 <k.sumit@hotmail.com>
2020-05-26 11:40:45 +1000

    Create rollout.ps1

---
+++ rollout.ps1
@@ -0,0 +1,81 @@
+﻿
+
+$hub = [SubscriptionDetails]::new("3015bf6b-c772-408e-91dc-9e92670794d7","aehubrsg01")
+$preProd = [SubscriptionDetails]::new("3015bf6b-c772-408e-91dc-9e92670794d7","aepreprodrsg01")
+$prod = [SubscriptionDetails]::new("3015bf6b-c772-408e-91dc-9e92670794d7","aeprodrsg01")
+$nonProd = [SubscriptionDetails]::new("3015bf6b-c772-408e-91dc-9e92670794d7","aenonprodrsg01")
+
+Class SubscriptionDetails
+{
+    [String]$_subscriptionId
+    [string]$_resourceGroup
+    #[String]$_spnUser
+    #[string]$_spnPassword
+    #[String]$_tenantId
+    #SubscriptionDetails([String]$SubscriptionId,[string]$ResourceGroup,[String]$SpnUser,[string]$SpnPassword,[String]$tenantId)
+    SubscriptionDetails([String]$SubscriptionId,[string]$ResourceGroup)
+    {
+        $this._subscriptionId = $SubscriptionId
+        $this._resourceGroup = $ResourceGroup
+        #$this._spnUser = $SpnUser
+        #$this._spnPassword = $SpnPassword
+        #$this._tenantId = $tenantId
+    }
+}
+
+<# Login #>
+<#
+Function Login($subscriptionDetails) {
+    $psCred = New-Object System.Management.Automation.PSCredential($subscriptionDetails._spnUser , (ConvertTo-SecureString $subscriptionDetails._spnPassword -AsPlainText -Force) )
+    Connect-AzAccount -Credential $psCred -TenantId $subscriptionDetails._tenantId  -ServicePrincipal 
+}
+
+
+Function Logout(){
+    Disconnect-AzAccount
+} 
+#>
+
+<# rollout nonprod #>
+#Login($nonProd)
+#New-AzResourceGroupDeployment -ResourceGroupName $nonProd._resourceGroup -TemplateFile nonprod/azureDeploy_nonprod_vnet.json -verbose
+$nonProdVnetObject = Get-AzVirtualNetwork -ResourceGroupName $nonProd._resourceGroup -verbose
+#Logout 
+
+<# rollout preprod #>
+#Login($preProd) 
+#New-AzResourceGroupDeployment -ResourceGroupName $preProd._resourceGroup -TemplateFile preprod/azureDeploy_preprod_vnet.json -verbose
+$preProdVnetObject = Get-AzVirtualNetwork -ResourceGroupName $preProd._resourceGroup
+#Logout
+
+<# rollout prod #>
+#Login($prod)
+#New-AzResourceGroupDeployment -ResourceGroupName $Prod._resourceGroup -TemplateFile prod/azureDeploy_prod_vnet.json -verbose
+$prodVnetObject = Get-AzVirtualNetwork -ResourceGroupName $prod._resourceGroup
+#Logout
+
+<# rollout hub #>
+#Login($hub)
+#New-AzResourceGroupDeployment -ResourceGroupName $hub._resourceGroup -TemplateFile hub/azureDeploy_hub.json -verbose
+<# peering hub with other networks #>
+$hubVnetObject = Get-AzVirtualNetwork -ResourceGroupName $hub._resourceGroup
+#Add-AzVirtualNetworkPeering -Name ($hubVnetObject.name + 'to' + $prodVnetObject.name) -VirtualNetwork $hubVnetObject -RemoteVirtualNetworkId $prodVnetObject.Id -AllowGatewayTransit
+#Add-AzVirtualNetworkPeering -Name ($hubVnetObject.name + 'to' + $preProdVnetObject.name) -VirtualNetwork $hubVnetObject -RemoteVirtualNetworkId $preProdVnetObject.Id -AllowGatewayTransit
+Add-AzVirtualNetworkPeering -Name ($hubVnetObject.name + 'to' + $nonProdVnetObject.name) -VirtualNetwork $hubVnetObject -RemoteVirtualNetworkId $nonProdVnetObject.Id -AllowGatewayTransit
+#Logout
+
+<# peering other networks with hub#>
+<# rollout nonprod #>
+#Login($nonProd)
+Add-AzVirtualNetworkPeering -Name $hubVnetObject.name -VirtualNetwork $nonProdVnetObject -RemoteVirtualNetworkId $hubVnetObject.Id -UseRemoteGateways
+#Logout
+
+<# rollout preprod #>
+#Login($preProd)
+Add-AzVirtualNetworkPeering -Name $hubVnetObject.name -VirtualNetwork $preProdVnetObject -RemoteVirtualNetworkId $hubVnetObject.Id -UseRemoteGateways
+#Logout
+
+<# rollout prod #>
+#Login($prod)
+Add-AzVirtualNetworkPeering -Name $hubVnetObject.name -VirtualNetwork $prodVnetObject -RemoteVirtualNetworkId $hubVnetObject.Id -UseRemoteGateways
+#Logout
